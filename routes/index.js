ad7aecb7bc2387b15a823f3da16b74bf2fd135c6
Sarah <git@imsarah.ca>
2022-10-30 20:06:15 -0400

    Initial Commit for working website

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About' });
+});
+
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact' });
+});
+
+router.get('/contact/success', function(req, res, next) {
+  res.render('success', { title: 'Success' });
+});
+module.exports = router;
+
